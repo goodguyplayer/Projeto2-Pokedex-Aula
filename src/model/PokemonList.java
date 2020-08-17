c37e9833d8640ec4477919ab0fcf6972285d8505
goodguyplayer <42227590+goodguyplayer@users.noreply.github.com>
2020-08-17 16:34:58 -0300

    Update PokemonList.java

--- src/model/PokemonList.java
+++ src/model/PokemonList.java
@@ -1,6 +1,24 @@
 package model;
 
+import java.util.ArrayList;
+import java.util.List;
+
 // List of pokemons. Congats! You caught them all!
 public class PokemonList {
-    
+    private List<Pokemon> pokemons;
+
+    // Constructor
+    public PokemonList() {
+        this.pokemons = new ArrayList<>();
+    }
+
+    // Getter
+    public List<Pokemon> getPokemons() {
+        return pokemons;
+    }
+
+    // add Pokemon
+    public void addPokemon(Pokemon pokemon){
+        this.pokemons.add(pokemon);
+    }
 }
