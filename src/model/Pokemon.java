18f7e16b92b9a93eafc1fe3bea3f401236c8a76a
goodguyplayer <42227590+goodguyplayer@users.noreply.github.com>
2020-08-17 16:26:13 -0300

    Class Pokemon updated - Added var, constructor, getter and toString

--- src/model/Pokemon.java
+++ src/model/Pokemon.java
@@ -2,4 +2,62 @@ package model;
 
 // Represents the pokemon
 public class Pokemon {
+    String name;
+    int height;
+    int weight;
+    int id;
+    String type;
+    String sprite;
+    String description;
+
+    public Pokemon(String name, int height, int weight, int id, String type, String sprite, String description) {
+        this.name = name;
+        this.height = height;
+        this.weight = weight;
+        this.id = id;
+        this.type = type;
+        this.sprite = sprite;
+        this.description = description;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public int getWeight() {
+        return weight;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public String getSprite() {
+        return sprite;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    @Override
+    public String toString() {
+        return "Pokemon{" +
+                "name='" + name + '\'' +
+                ", height=" + height +
+                ", weight=" + weight +
+                ", id=" + id +
+                ", type='" + type + '\'' +
+                ", sprite='" + sprite + '\'' +
+                ", description='" + description + '\'' +
+                '}';
+    }
 }
