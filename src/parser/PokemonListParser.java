dfe5d05768ceb02ce054a64c38d421e9bad03b3a
goodguyplayer <42227590+goodguyplayer@users.noreply.github.com>
2020-08-17 16:37:39 -0300

    Update PokemonListParser.java

--- src/parser/PokemonListParser.java
+++ src/parser/PokemonListParser.java
@@ -1,4 +1,23 @@
 package parser;
 
+import model.PokemonList;
+import org.json.JSONArray;
+import org.json.JSONObject;
+
 public class PokemonListParser {
+    public static JSONArray toJson(PokemonList list){
+        JSONArray json = new JSONArray();
+        list.getPokemons().forEach( pokemon -> {
+            json.put(PokemonParser.toJson(pokemon));
+        });
+        return json;
+    }
+
+    public static PokemonList fromJson(JSONArray json){
+        PokemonList list = new PokemonList();
+        json.forEach( item -> {
+            list.addPokemon(PokemonParser.fromJson((JSONObject) item));
+        });
+        return list;
+    }
 }
