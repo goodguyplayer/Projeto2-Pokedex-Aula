de2f806e5a9abf7c29719c9b183bf48bdd85be04
goodguyplayer <42227590+goodguyplayer@users.noreply.github.com>
2020-08-17 16:32:05 -0300

    Updated PokemonParser

--- src/parser/PokemonParser.java
+++ src/parser/PokemonParser.java
@@ -1,4 +1,32 @@
 package parser;
 
+import model.Pokemon;
+import org.json.JSONObject;
+
 public class PokemonParser {
+    public static JSONObject toJson(Pokemon pokemon){
+        JSONObject json = new JSONObject();
+        json.put("name", pokemon.getName());
+        json.put("height", pokemon.getHeight());
+        json.put("weight", pokemon.getWeight());
+        json.put("id", pokemon.getId());
+        json.put("type", pokemon.getType());
+        json.put("sprite", pokemon.getSprite());
+        json.put("description", pokemon.getDescription());
+
+        return json;
+    }
+
+    public static Pokemon fromJson(JSONObject json){
+        Pokemon pokemon = new Pokemon(
+                json.getString("name"),
+                json.getInt("height"),
+                json.getInt("weight"),
+                json.getInt("id"),
+                json.getString("type"),
+                json.getString("sprite"),
+                json.getString("description")
+        );
+        return pokemon;
+    }
 }
